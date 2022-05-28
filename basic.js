1a53498c72ba9a6b01e0317675e4f3b5f5767cbc
sunny dalvi <55959027+sunnydalvi94@users.noreply.github.com>
2022-05-28 16:54:14 +0530

    basic.html file added

--- basic.js
+++ basic.js
@@ -1,5 +1,5 @@
-let fno=prompt("Enter First number");
-let sno=prompt("Enter second number");
-let result = parseInt(fno)+parseInt(sno);
+let fno = prompt("Enter First number");
+let sno = prompt("Enter second number");
+let result = parseInt(fno) + parseInt(sno);
 
-alert("addition of two number is: "+result);
+alert("addition of two number is: " + result);
